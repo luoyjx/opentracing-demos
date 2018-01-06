429ffc28e1851298abfcc56fcebb4569ed0c537a
luoyjx <yanjixiong@shimo.im>
2018-01-07 00:35:24 +0800

    feat: koa@1 demo

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # opentracing-demos
 opentracing demo for node.js
+
+## table of contents
+* koa1 (using fork of [opentracing-auto](https://github.com/luoyjx/opentracing-auto))
---
+++ koa1/server2.js
@@ -0,0 +1,27 @@
+const http = require('http')
+const Instrument = require('@risingstack/opentracing-auto')
+const jaeger = require('jaeger-client')
+const UDPSender = require('jaeger-client/dist/src/reporters/udp_sender').default
+
+const sampler = new jaeger.RateLimitingSampler(1)
+const reporter = new jaeger.RemoteReporter(new UDPSender({ host: '121.40.129.45' }))
+const tracer = new jaeger.Tracer('serviceB', reporter, sampler, {
+  tags: {
+    gitTag: 'foo'
+  }
+})
+
+const instrument = new Instrument({
+  tracers: [tracer]
+})
+
+const koa = require('koa')
+const app = koa()
+const rp = require('request-promise')
+
+app.use(function * () {
+  yield rp('https://shimo.im')
+  this.body = { success: true }
+})
+
+app.listen(3001)
